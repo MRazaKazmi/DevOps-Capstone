c0ee998879403abfc2c94d20335076b7ff335c27
Raza Kazmi <razakazmi@Razas-MacBook-Pro-2.local>
2022-06-19 00:20:48 +0500

    Update CI config file

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -7,7 +7,7 @@ dockerpath=230793/udacity-project-5
 # Step 2: Authenticate & tag
 
 docker tag udacity-project-5 230793/udacity-project-5
-docker login --username=230793
+docker login --username 230793 --password $DOCKER_PASSWORD
 
 # Step 3: Push image to a docker repository
 docker push 230793/udacity-project-5
